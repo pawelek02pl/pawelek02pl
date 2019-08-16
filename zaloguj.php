dc6a668a6d12016390474b2cfd1066c521265dfd
pawelek02pl <klaczynski.pawel@gmail.com>
2019-08-17 00:52:08 +0200

    a11

---
+++ zaloguj.php
@@ -0,0 +1,94 @@
+
+
+<?php
+
+ session_start();
+ 
+ if ((!isset($_POST['login'])) || (!isset($_POST['haslo'])))
+ {
+ header('Location:index.php;');
+ exit();
+ }
+ 
+require_once "connect.php";
+
+$polaczenie = @new mysqli($host,$db_user,$db_password,$db_name);
+
+if($polaczenie->connect_errno!=0)
+{
+echo "error".$polaczenie->connect_errno;
+
+}
+else
+{
+	$login=$_POST['login'];
+	$haslo=$_POST['haslo'];
+	
+	$login= htmlentities($login, END_QUOTES, "UTF-8");
+	$haslo= htmlentities($haslo, END_QUOTES, "UTF-8");
+	
+	
+	
+	
+	if($rezultat =@$polaczenie->query(sprintf("SELECT * FROM uzytkownicy WHERE user='%s' AND pass='%s'",
+	mysqli_real_escape_string($polaczenie,$login),
+	mysqli_real_escape_string($polaczenie,$haslo))))
+	{
+		$ilu_userow = $rezultat->num_rows;
+		if($ilu_userow>0)
+		{
+			
+			$_SESSION['zalogowany'] = true;
+			
+			
+			$wiersz = $rezultat->fetch_assoc();
+			$_SDESSION['id'] = $wiersz['id'];
+			$_SESSION['user'] = $wiersz['user'];
+			$_SESSION['drewno'] = $wiersz['drewno'];
+			$_SESSION['kamien'] = $wiersz['kamien'];
+			$_SESSION['zboze'] = $wiersz['zboze'];
+			$_SESSION['email'] = $wiersz['email'];
+			$_SESSION['dnipremium'] = $wiersz['dnipremium'];
+			
+			unset($_SESSION['blad']);
+			
+			
+			
+			
+			
+			$rezultat->free_result();
+			
+			header('Location:gra.php');
+		}
+		else
+		{
+			
+			$_SESSION['blad'] = '<span style="color:red;">Nieprawidłoowy login lub chasło!</span>';
+			header('Location: index.php');
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+	}
+
+	
+	
+	
+	$polaczenie->close();
+}
+
+
+
+
+
+
+
+
+
+
+?>
